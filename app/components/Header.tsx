61a53c4639f8737c20cfacb67decfa3d243e795f
minoru horiuchi <noru238@gmail.com>
2023-12-16 11:39:29 +0900

    Headerの追加

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export function Header() {
+    return (
+    <footer className="w-1/6 flex justify-around ">
+            <Link className="hover:text-amber-300" href="/"> Index </Link>
+            <Link className="hover:text-amber-300" href="/about"> About </Link>
+        </footer>
+    )
+}
