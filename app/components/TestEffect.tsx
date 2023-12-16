b25cb3bceecaf2dfc297126d58eab9a524ef049d
minoru horiuchi <noru238@gmail.com>
2023-12-16 18:35:28 +0900

    usePath実装

--- app/components/TestEffect.tsx
+++ app/components/TestEffect.tsx
@@ -1,14 +1,25 @@
 "use client";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export function TestEffect() {
+  const pathName = usePathname();
+  const bgColor = (pathName: string) => {
+    switch (pathName) {
+      case "/":
+        return "blue";
+      case "/about":
+        return "lightblue";
+      default:
+        return "";
+    }
+  };
   useEffect(() => {
-    //マウント時の処理
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = bgColor(pathName);
     return () => {
       //アンマウント時の処理
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [pathName]);
   return <div></div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import { Footer } from "@/app/components/Footer";
 import { Headline } from "@/app/components/Headline";
 import { Header } from "@/app/components/Header";
 import { TestState } from "@/app/components/TestState";
+import { TestEffect } from "@/app/components/TestEffect";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <TestEffect />
       <Header />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Headline title="index page" comp={<div>here</div>}>
@@ -17,7 +19,6 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         this is index
       </div>
-
       <Footer />
     </main>
   );
