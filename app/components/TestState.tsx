b2237f15054d5f088797ddc1472975b5fa7a2b42
minoru horiuchi <noru238@gmail.com>
2023-12-16 15:11:22 +0900

    state色々実装

--- app/components/TestState.tsx
+++ app/components/TestState.tsx
@@ -1,20 +1,45 @@
 "use client";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 export function TestState() {
   const [count, setCount] = useState(0);
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
+  const handleClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
     setCount((count) => count + 1);
-  };
+  }, []);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setText((text) => e.target.value);
+  }, []);
+
+  const toggleIsShow = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   return (
     <div>
-      <div className="flex justify-center">{count}</div>
+      <div className="flex justify-center h-7">{isShow ? count : null}</div>
+
       <button
         className="flex justify-center text-blue-600"
         onClick={handleClick}
       >
         click me（indexページ）
       </button>
+      <button
+        className="mt-3 w-full flex justify-center bg-blue-900 text-black hover:bg-blue-400"
+        onClick={toggleIsShow}
+      >
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <label className="block mt-3">お名前</label>
+      <input
+        className="text-black w-full flex justify-center mt-1"
+        value={text}
+        onChange={handleChange}
+      />
     </div>
   );
 }
